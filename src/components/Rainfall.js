b81f9046c1d5babfde7ef925d7a2eafcd2bb122c
Mark Terpstra <mark.terpstra.mt@gmail.com>
2025-10-05 16:37:43 -0400

    Add Area to Rainfall Data List

--- src/components/Rainfall.js
+++ src/components/Rainfall.js
@@ -4,9 +4,10 @@ export const Rainfall = (props) => {
   const collection = "rain";
   const columns = [
     {name:"Amount", selector: row => `${row.amount}mm`, sortable:true},
-    {name:"Date", selector: row => row.date, sortable:true},
-    {name:"Who", selector: row => row.name, sortable:true},
-    {name:"From", selector: row => row.phone, sortable:true},
+    {name:"Date",   selector: row => row.date,  sortable:true},
+    {name:"Area",   selector: row => row.area,  sortable:true},
+    {name:"Who",    selector: row => row.name,  sortable:true},
+    {name:"From",   selector: row => row.phone, sortable:true},
   ];
   return (
     <DataCollection 
