1b693dfd7f32a6ba690dc706fec633103a70963d
Mark Terpstra <mark.terpstra.mt@gmail.com>
2026-01-10 10:28:14 -0500

    Work in Progress of Bar Chart Version of Temperature Data

--- src/components/TemperatureChart.js
+++ src/components/TemperatureChart.js
@@ -1,39 +1,107 @@
-import DataCollectionPieChart from './DataCollectionPieChart.js';
+import React, { useState, useEffect } from 'react';
+import { Fetch } from './Utils.js';
 import { useTranslation } from 'react-i18next';
+
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+);
+
 export const TemperatureChart = (props) => {
   const { t } = useTranslation();
-  const aggregateData = (rawData) => {
-    var xValues = [];
-    var yValues = [];
-    var counts = [];
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    Fetch("/api/data/temperatures", "GET", null, 
+      (data) => {
+        setData(data);
+      })
+  }, []);
+
+
+  const getLabels = (rawData) => {
+    var labels = [];
     for(let i=0; i < rawData.length; i++) {
       const yearMonth = rawData[i].date.slice(0, 7);
-      let index = xValues.indexOf(yearMonth);
+      let index = labels.indexOf(yearMonth);
       if (index < 0) {
-        xValues.push(yearMonth);
-        yValues.push(rawData[i]["temperature"]);
-        counts.push(1);
-      } else {
-        yValues[index] += rawData[i]["temperature"];
-        counts[index]++;
-      }
+        labels.push(yearMonth);
+      } 
     }
-
-    // We are doing average, not totals.
-    for(let i=0; i < yValues.length; i++) {
-      yValues[i] = yValues[i] / counts[i];
-    }
-
-    return {xValues:xValues, yValues:yValues}
+    return labels;
   }
+
+  const aggregateData = (rawData) => {
+    var agg = {};
+    for(let i=0; i < rawData.length; i++) {
+      const yearMonth = rawData[i].date.slice(0, 7);
+      const area = rawData[i]["area"];
+      if (agg[yearMonth] === undefined) {
+        agg[yearMonth] = {};
+      }
+      if (agg[yearMonth]["all_areas"] === undefined) {
+        agg[yearMonth]["all_areas"] = {total:0, count:0};
+      }
+      if (agg[yearMonth][area] === undefined) {
+        agg[yearMonth][area] = {total:0, count:0};
+      }
+      agg[yearMonth]["all_areas"].total += rawData[i]["temperature"];
+      agg[yearMonth]["all_areas"].count++;
+      agg[yearMonth][area].total += rawData[i]["temperature"];
+      agg[yearMonth][area].count++;
+    }
+    return agg;
+  }
+
+
+
+  const graphData = aggregateData(data);
+  const labels = graphData.months;
+
+  const thedata = {
+    labels,
+    datasets: [],
+  };
+
+
+  for (const [key, value] of Object.entries(graphData)) {
+    console.log(`${key}: ${value}`);
+    thedata.datasets.push(
+      {
+        label: `Average Rainfall by ${key}`,
+        data: value.all_areas,
+        backgroundColor: "#BAA898",
+      }
+    )
+  }
+
+
+
+
+
+
   return (
-    <DataCollectionPieChart
-      collection="temperatures"
-      aggregateData={aggregateData}
-      title={t("temperature_by_month")}
-      colors={["#7F3500", "#0B7F00", "#004A7F", "#74007F"]}
-    />
+    <div className="Chart">
+      <h3>Average Rainfall by Month</h3>
+      <Bar data={thedata} />
+    </div>
   );
 
 }
 export default TemperatureChart;
+
+
