f498686e8ace3a929851c2bef2aca0136f0244a5
Mark Terpstra <mark.terpstra.mt@gmail.com>
2026-01-14 19:08:38 -0500

    Adding four new breeds

--- src/components/BreedDropdown.js
+++ src/components/BreedDropdown.js
@@ -11,11 +11,15 @@ export const BreedDropdown = (props) => {
       {!props.selected && 
        <option value="" disabled selected hidden>-- Choose an Breed --</option>
       }
-      <Option value="angus"     name="Angus"     selected={props.selected}/>
-      <Option value="nelore"    name="Nelore"    selected={props.selected}/>
-      <Option value="brangus"   name="Brangus"   selected={props.selected}/>
-      <Option value="sta_zelia" name="Sta Zelia" selected={props.selected}/>
-      <Option value="cruzada"   name="Cruzada"   selected={props.selected}/>
+      <Option value="angus"        name="Angus"        selected={props.selected}/>
+      <Option value="nelore"       name="Nelore"       selected={props.selected}/>
+      <Option value="brangus"      name="Brangus"      selected={props.selected}/>
+      <Option value="sta_zelia"    name="Sta Zelia"    selected={props.selected}/>
+      <Option value="cruzada"      name="Cruzada"      selected={props.selected}/>
+      <Option value="murrah"       name="Murrah"       selected={props.selected}/>
+      <Option value="mediterrâneo" name="Mediterrâneo" selected={props.selected}/>
+      <Option value="jafarabadi"   name="Jafarabadi"   selected={props.selected}/>
+      <Option value="carabao"      name="Carabao"      selected={props.selected}/>
     </select>
   );
 }
